b9d579b93bcef891ce9f6e5cadd2362ca0a67265
Ayushi Agrawal <ayushiagrawal2507@gmail.com>
2026-01-05 12:05:46 +0530

    Add files via upload

---
+++ HOTEL/app0.py
@@ -0,0 +1,106 @@
+from flask import *
+
+app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('hote.html')
+
+@app.route('/book')
+def bookingload():
+    return render_template('hotelloginpage.html',mes='')
+
+details={}
+
+
+@app.route('/check',methods=['POST','GET'])
+def check():
+    if request.method=="POST":
+        user=request.form['username1']
+        pasw=request.form['pass1']
+        if user in details and details[user][0]==pasw:
+            return render_template('dashboard.html',name=details[user][1])
+        else:
+            return render_template('hotelloginpage.html',mes='Invalid username and password')
+
+
+@app.route('/add',methods=['POST','GET'])
+def adding():
+    if request.method=="POST":
+        user=request.form['username2']
+        pasw=request.form['pass2']
+        n=request.form['nam']
+        details[user]=[pasw,n]
+        return render_template('hotelloginpage.html',mes='')
+
+app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import *
+# from datetime import timedelta
+# app=Flask(__name__)
+
+# app.secret_key = "secret123"
+# app.permanent_session_lifetime = timedelta(minutes=1)
+
+# @app.route('/login')
+# def login():
+#     session.permanent = True
+#     session['user_id'] = 1
+#     return "Logged in for 1 minutes"
+
+# @app.errorhandler(406)
+# def forbidden(e):
+#     return "Username cannot start with number", 403
+
+
+# @app.route('/l')
+# def g():
+#     abort(406,'bdbkwdw')
+
+# @app.route('/')
+# def h():
+#     return render_template('sigup.html')
+# @app.route('/set',methods=['POST','GET'])
+# def k():
+#     if request.method=='POST':
+#         a=request.form
+#         an=make_response(render_template('logi.html'))
+#         an.set_cookie('username',a['user'])
+#         an.set_cookie('password',a['pass'])
+#         return an
+# @app.route('/log',methods=["POST","GET"])
+# def n():
+#     if request.method=='POST':
+#         a=request.form
+#         if a['user']==request.cookies.get('username') and a['pass']==request.cookies.get('password'):
+#             return redirect(url_for(login))
+#         return "Failure"
+    
+# app.run(debug=True)
+
+
+# ############################################3
+
+
+
