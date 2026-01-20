bec78d0b7742d010f1273d4392198e40de094bb0
ayushi-ag-25 <ayushiagrawal2507@gmail.com>
2026-01-20 22:16:32 +0530

    Final Railway working version

--- app.py
+++ app.py
@@ -249,5 +249,5 @@ def profile():
 
 # ---------------- START ----------------
 init_db()  # run this for Railway/Gunicorn
-port = int(os.environ.get("PORT", 8080))
+port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
