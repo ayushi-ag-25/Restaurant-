b68691474ff54b2c3b7ab235958a20119de30d5c
ayushi-ag-25 <ayushiagrawal2507@gmail.com>
2026-01-20 10:22:00 +0530

    Render deployment ready

--- app0.py
+++ app0.py
@@ -189,9 +189,12 @@ def cedit():
 
 
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment, fallback to 5000 locally
-    app.run(host="0.0.0.0", port=port,debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
