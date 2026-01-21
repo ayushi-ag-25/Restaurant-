63a2a4655b15c4bfa8f7a5c498b7ed957f5195fd
ayushi-ag-25 <ayushiagrawal2507@gmail.com>
2026-01-21 12:09:31 +0530

    latest changes

--- app.py
+++ app.py
@@ -280,6 +280,12 @@ def cedit():
     data=cursor.fetchone()
     return render_template('editprofile.html',d=data)
 
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, private'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 # ---------------- START ----------------
 init_db()  # run this for Railway/Gunicorn B 
 
